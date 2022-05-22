270f1ee7ed3a1d17a87c049ab38d110e83ec1c46
Behrouz Roameh <behrouz.rozmeh@gmail.com>
2022-05-23 01:20:07 +0900

    NotFound.module.css are added for styling NotFound.js

--- my-app/src/component/NotFound.js
+++ my-app/src/component/NotFound.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
+import style from "./NotFound.module.css";
 
 class NotFound extends Component {
     render() {
         return (
-            <div>
+            <div className={style.container}>
                 <h1>Not Found</h1>
             </div>
         );
